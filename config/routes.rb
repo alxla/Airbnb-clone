952f9482981dc0fa133d772e3d57c6cdd3d49b95
Alexandre Langlais <alxlanglais@gmail.com>
2017-03-01 11:54:12 +0100

    Routes and Universes controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  resources :universes, only: [:index, :show]
+
+  # get    "universes",          to: "universes#index"
+  # get    "universes/:id",      to: "universes#show"
+
+  root to: 'universes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
