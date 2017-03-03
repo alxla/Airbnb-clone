3beb6ab1ee610111264865d3e768b28f182f221c
Marc Loher <marc.loher@interfaceurope.eu>
2017-03-03 14:20:09 +0100

    user story sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:home, :contact]
+
   def home
   end
+
+  def contact
+  end
+
+  def signup
+  end
+
 end
